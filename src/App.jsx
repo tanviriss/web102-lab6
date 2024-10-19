901dce1b8bcf496a3b172df05486359e458a357d
Tanvir <Tanvirislamnyc@gmail.com>
2024-10-18 22:55:57 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+function App() {
+  const [list, setList] = useState(null)
+
+  useEffect(() => {
+    const fetchAllCoinData = async() => {
+      
+    }
+  }, [])
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
